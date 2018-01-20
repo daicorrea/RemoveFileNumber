a5ddb9b63e4cf017d3b9fba3e79b44744f3fe99b
Daiane Correa <daoli@br.ibm.com>
2018-01-19 22:42:25 -0200

    Adding Rename Print

--- rename-file.py
+++ rename-file.py
@@ -9,8 +9,11 @@ def rename_files():
     os.chdir('./prank')
     #(2) Rename files in a loop
     for file_name in file_list:
+        print('Renaming: "' + file_name + '"')
+        new_file_name = file_name.translate(None, '0123456789')
         #Using the translate function to remove numbers
-        os.rename(file_name, file_name.translate(None, '0123456789'))
+        os.rename(file_name, new_file_name)
+        print('to: "' + new_file_name + '""')
 
 
 rename_files()
