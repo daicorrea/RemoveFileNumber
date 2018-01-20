aebc4244c0436e5d65dd59de3ce88e082f6edfa9
Daiane Correa <daoli@br.ibm.com>
2018-01-19 22:38:22 -0200

    Changin Working Directory

--- rename-file.py
+++ rename-file.py
@@ -1,8 +1,16 @@
 import os
 
 def rename_files():
-    #get file names from folder
+    #(1) get file names from folder
     file_list = os.listdir('./prank')
+    #print file list
     print(file_list)
+    #change current working directory to rename files
+    os.chdir('./prank')
+    #(2) Rename files in a loop
+    for file_name in file_list:
+        #Using the translate function to remove numbers
+        os.rename(file_name, file_name.translate(None, '0123456789'))
+
 
 rename_files()
